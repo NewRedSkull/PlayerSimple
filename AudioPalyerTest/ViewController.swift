9da6d2be6b649a82c990d54c565946f72c4fe6e6
Andrii <andrii@MacBook-Pro-Andrii.local>
2018-11-29 13:01:08 +0200

    New

--- AudioPalyerTest/ViewController.swift
+++ AudioPalyerTest/ViewController.swift
@@ -6,13 +6,34 @@
 //  Copyright © 2018 Andrii. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    @IBAction func Play(_ sender: UIButton) {
+        player.play()
+    }
+    @IBAction func Stop(_ sender: UIButton) {
+        player.stop()
+    }
+    @IBAction func Repaly(_ sender: UIButton) {
+        player.currentTime = 0 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+         let audioPath = Bundle.main.path(forResource: "song", ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+        }catch{
+            //ERRORS
+        }
+        
     }
 
 
